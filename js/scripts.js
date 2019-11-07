d129e5493d19f3e94c3fd2293f2b436e90370e34
Nick ACCD <nickpetrilla@gmail.com>
2019-11-06 16:24:58 -0800

    Some JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log('Hello from js/scripts.js!');
+
+var displayedImage = document.querySelector('.displayed-img');
+var thumbView = document.querySelector('.view-image');
+
+var darkBtn = document.querySelector('dark');
+var overlay = document.querySelector('.overlay');
+
+
+function changeImage() {
+
+}
+
+
+
+
+thumbView.onclick = function(changeImage) {
+
+console.log('thumb');
+
+}
