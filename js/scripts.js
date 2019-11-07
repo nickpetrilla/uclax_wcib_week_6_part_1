cf595d810e7c90812c87b1f7c7701025205f147e
Nick ACCD <nickpetrilla@gmail.com>
2019-11-07 09:58:06 -0800

    JS update, still not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 console.log('Hello from js/scripts.js!');
 
 var displayedImage = document.querySelector('.displayed-img');
-var thumbView = document.querySelector('.view-image');
+var thumbBar = document.querySelector('.thumb-bar');
 
-var darkBtn = document.querySelector('dark');
+var btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 
-function changeImage() {
-
+for(var i = 1; i <= 8; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    var imgSrc = e.target.getAttribute('src');
+    displayImage(imgSrc);
+  }
 }
 
+function displayImage(imgSrc) {
+  displayedImage.setAttribute('src', imgSrc);
+}
 
-
-
-thumbView.onclick = function(changeImage) {
-
-console.log('thumb');
-
+btn.onclick = function() {
+  var btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    console.log('Dark Mode');
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
 }
